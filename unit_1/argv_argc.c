4e2faa0280a7b5202f12e687934a6af895ef5f74
MERLIN-ARCH <uady2021@gmail.com>
2022-02-09 11:46:46 -0600

    argv task

--- unit_1/argv_argc.c
+++ unit_1/argv_argc.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main(int argc, char** argv){
-    printf("Programa ejectuado: %s,number of inputs: %d", argv[0], (argc-1));
+    if (argc <= 1){
+        printf("Entrada sin argumentos, saliendo...\n");    
+        return 0;
+    }
+    printf("Programa ejectuado: %s, number of inputs: %d\n", argv[0], (argc-1));
     printf("Bienvenido usuario: %s %s %s %s, conexion establecida\n", argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
